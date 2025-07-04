d6aa135b37379f4e40d1613e50f4edbde262d81a
AMIRALI DANESHVAR <123299462+mehranditor@users.noreply.github.com>
2025-07-04 23:26:48 +0300

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -3,11 +3,11 @@ import 'dart:io' show Platform;
 class Config {
   static String get apiUrl {
     if (Platform.isAndroid) {
-      return 'http://10.0.2.2:5000/api/chat';
+      return '';
     } else if (Platform.isIOS) {
-      return 'http://localhost:5000/api/chat';
+      return '';
     } else {
-      return 'http://192.168.1.100:5000/api/chat';
+      return '';
     }
   }
 }
